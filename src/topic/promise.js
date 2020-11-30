44f76fc0a2ef931f7ef7ac006de1784f7f6531eb
qxl <qinxuelian@bonc.com.cn>
2020-11-30 17:52:30 +0800

    promise.race 的使用方法

--- src/topic/promise.js
+++ src/topic/promise.js
@@ -38,3 +38,20 @@ function fn() {
         console.log(res); // 再过1000ms后输出3
     });
 }
+
+
+// 3.promise.race 的用法
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 500, 'one');
+  });
+  
+  const promise2 = new Promise((resolve, reject) => {
+    setTimeout(resolve, 100, 'two');
+  });
+  
+  Promise.race([promise1, promise2]).then((value) => {
+    console.log(value);
+    // Both resolve, but promise2 is faster
+  });
+  // expected output: "two"
+  
