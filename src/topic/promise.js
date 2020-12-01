4b4c384f6ed6f63e09a29c2609ec8b339ed40c8b
qxl <qinxuelian@bonc.com.cn>
2020-12-01 10:27:25 +0800

    promise 的基本用法

--- src/topic/promise.js
+++ src/topic/promise.js
@@ -1,29 +1,50 @@
 // // 1. promise 用法
 
-// function loadImg(src){
-//     let img = document.createElement('img')
-//     let promise = new Promise((resolve, reject) => {
-//         img.onsuccess = function(){
-//             resolve(img)
-//         } 
-//         img.onsuccess = function(){
-//             reject('error')
-//         }
-//         img.src = src
-//     })
-//     return promise
-// }
+// （1）原始的回调函数方式
+function loadImg (src, callback, fail) {
+    let img = document.createElement('img')
+    img.onload = function () {
+        callback(img)
+    }
+    img.onerror = function () {
+        fail()
+    }
+    img.src = src
+}
 
-// 2
-function fn() {
-    function p1() {
+let src = 'http://abc.png'
+loadImg(src, function (img) {
+    console.log(img.width);
+}, function () {
+    console.log('load image failed!');
+})
+
+
+// （2）使用 promise 的方式
+function loadImg (src) {
+    let promise = new Promise((resolve, reject) => {
+        let img = document.createElement('img')
+        img.onload = function () {
+            resolve(img)
+        }
+        img.onerror = function () {
+            reject('error')
+        }
+        img.src = src
+    })
+    return promise
+}
+
+// 2. Promise 的用法2: 链式调用
+function fn () {
+    function p1 () {
         return new Promise(function (resolve, reject) {
             setTimeout(function () {
                 resolve(1);
             }, 1000);
         });
     }
-    function p2(value) {
+    function p2 (value) {
         return new Promise(function (resolve, reject) {
             setTimeout(function () {
                 console.log('2')
@@ -40,18 +61,18 @@ function fn() {
 }
 
 
-// 3.promise.race 的用法
+// 3.promise.race 的用法  https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/race
 const promise1 = new Promise((resolve, reject) => {
     setTimeout(resolve, 500, 'one');
-  });
-  
-  const promise2 = new Promise((resolve, reject) => {
+});
+
+const promise2 = new Promise((resolve, reject) => {
     setTimeout(resolve, 100, 'two');
-  });
-  
-  Promise.race([promise1, promise2]).then((value) => {
+});
+
+Promise.race([promise1, promise2]).then((value) => {
     console.log(value);
     // Both resolve, but promise2 is faster
-  });
+});
   // expected output: "two"
-  
+
