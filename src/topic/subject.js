2a99475074432c8aaad5ba664c6da3aca058eea5
qxl <qinxuelian@bonc.com.cn>
2020-12-26 15:19:23 +0800

    题目修改

---
+++ src/topic/subject.js
@@ -0,0 +1,17 @@
+// 各种题
+
+// 1.
+var a = new String('A');
+function showCase (data) {
+    switch (data) {
+        case 'A':
+            console.log(1);
+            break;
+        //中间还有一堆，我省略了
+        default:
+            console.log(5);
+            console.log(data);
+            console.log(typeof data);
+    }
+}
+showCase(a);
--- src/topic/type.js
+++ src/topic/type.js
@@ -1,8 +1,24 @@
 // type
 
+// 1.
 let a = true
 let b = typeof a
 let c = Object.prototype.toString.call(a)
 
 console.log(typeof b); // string
 console.log(c); // [object Boolean]
+
+
+// 2.
+function g1 () {
+    // console.log(typeof g1()) // 会报错
+    return 23;
+}
+let f = function g2 () {
+    return 23;
+}
+console.log(typeof g1());
+console.log(typeof f());
+console.log(typeof g2()); // 会报错，这种形式叫做命名的函数表达式，它的名字 g2 只在函数体内可见，在函数外部不可见。运行后 g 就没有了
+
+// 3.
