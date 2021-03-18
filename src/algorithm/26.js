9d855e61924d984fb30514eeb00867c6fd33557f
qxl <qinxuelian@bonc.com.cn>
2021-03-18 10:45:03 +0800

    算法刷题5，26

---
+++ src/algorithm/26.js
@@ -0,0 +1,51 @@
+// 26. 删除有序数组中的重复项
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  let preNum = null, pointer = 0
+  nums.sort(function (a, b) { return a - b }) // 不需要排序，题目说了有序
+  preNum = nums[0]
+  if (nums.length <= 0) {
+    return 0
+  }
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== preNum) {
+      nums[pointer] = preNum
+      preNum = nums[i]
+      pointer++
+    }
+    if (i === nums.length - 1) {
+      nums[pointer] = preNum
+      pointer
+    }
+  }
+  console.log(nums);
+  return pointer + 1
+};
+
+removeDuplicates([1, 3, 2, 1])
+
+
+// 借鉴：
+// 优点：不需要一个变量来记录上一次数据，直接不相等时赋值即可
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length == 0) {
+    return 0;
+  }
+  let lastIndex = 0;
+  for (let l = 1; l < nums.length; l++) {
+    if (nums[l] !== nums[lastIndex]) {
+      lastIndex++;
+      nums[lastIndex] = nums[l];
+    }
+  }
+  nums.length = lastIndex + 1; // 可以通过指定一个数组的长度来截取数组多少位之前的数据
+  return nums.length;
+};
---
+++ src/algorithm/5.js
@@ -0,0 +1,91 @@
+// 5.最长回文子串
+// 方法1 中心扩展 不准确
+let long = function (s) {
+  let left = 0, center = 0, right = 0, lenLeft = 0, lenRight = 0, lenBoth = 0, maxLen = 0, maxLeft = 0, maxRight = 0
+  if (s.length <= 0) {
+    return ''
+  }
+  if (s.length === 1) {
+    return s
+  }
+
+  function getPlainRound (left, right, center) {
+    let len = 0
+    if (!right) { // 向左
+      left--
+      while (left >= 0 && s[left] === s[center]) {
+        len++
+        left--
+      }
+    } else if (!left) { // 向右
+      right++
+      while (right <= s.length - 1 && s[right] === s[center]) {
+        len++
+        right++
+      }
+    } else {
+      left--
+      right++
+      while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+        len += 2
+        left--
+        right++
+      }
+    }
+    return len
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    left = right = center = i
+    while (left >= 0 && s[left] === s[center]) {
+      left--
+    }
+    while (right <= s.length - 1 && s[right] === s[center]) {
+      right++
+    }
+    while (left >= 0 && right <= s.length - 1 && s[left] === s[right]) {
+      left--
+      right++
+    }
+
+    if (right - left - 2 + 1 > maxLen) {
+      maxLen = right - left - 2 + 1 > maxLen ? right - left - 2 + 1 : maxLen
+      maxLeft = left + 1
+      maxRight = right - 1
+    }
+  }
+  console.log(s.slice(maxLeft, maxRight + 1))
+  return s.slice(maxLeft, maxRight + 1)
+}
+
+long("aaabaaaa")
+
+
+// 方法2 动态规划
+
+// 使用二维数组记录坐标，因为确实是下标比字符串更重要
+let dynamic = function () {
+  class Solution {
+    longestPalindrome (s) {
+      let n = s.length();
+      let dp = new boolean[n][n];
+      let ans = "";
+      for (let l = 0; l < n; ++l) { // l 是两数之间的距离
+        for (let i = 0; i + l < n; ++i) {
+          let j = i + l;
+          if (l == 0) {
+            dp[i][j] = true;
+          } else if (l == 1) {
+            dp[i][j] = (s.charAt(i) == s.charAt(j));
+          } else {
+            dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]); // 由于两数间距离是 l, l 从小变大的，所以一定能保证更短距离里的数都已经赋过值了
+          }
+          if (dp[i][j] && l + 1 > ans.length()) {
+            ans = s.substring(i, i + l + 1);
+          }
+        }
+      }
+      return ans;
+    }
+  }
+}
