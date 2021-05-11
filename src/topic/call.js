7b32cc1ab62d88dfa73b374f5bd67c7563616fe7
qxl <qinxuelian@bonc.com.cn>
2021-05-11 23:19:13 +0800

    修改 call.js 注释

--- src/topic/call.js
+++ src/topic/call.js
@@ -3,7 +3,7 @@
 
 // 1.第一版 给 foo 对象添加一个属性，这个属性就是函数 bar, 调用完再删掉即可。注意区别 bind 的实现， bind 实现里用了 call，而这里是要实现 call, 所以当然不能照抄 bind 的实现思路
 Function.prototype.call2 = function (context) {
-    // 首先要获取调用call的函数，用this可以获取
+    // 首先要获取调用call的函数，用this可以获取.整体思路是给 context 添加一个属性，这个属性就是 bar, 相当于把 bar 接到 context 的某个属性下，并调用
     context.fn = this;
     context.fn();
     delete context.fn;
@@ -21,14 +21,18 @@ function bar () {
 bar.call2(foo); // 1
 
 
+
+
 // 2.第二版 能够传递参数并调用
 Function.prototype.call2 = function(context) {
     context.fn = this;
     var args = [];
-    for(var i = 1, len = arguments.length; i < len; i++) {
+    for(var i = 1, len = arguments.length; i < len; i++) { // arguments 是个类数组，有迭代器，但不是真正的数组. 注意参数从第二个开始取，第一个只作为 this
         args.push('arguments[' + i + ']'); // 不能用 Array.prototype.slice.call 因为这里就是要实现 call
     }
-    eval('context.fn(' + args +')');
+    console.log('参数：', args); // ["arguments[1]", "arguments[2]"]
+    // eval('context.fn(' + args +')');
+    // context.fn(args) // 这样是无法正确调用的，必须按上面的方式写
     delete context.fn;
 }
 
@@ -47,6 +51,9 @@ bar.call2(foo, 'kevin', 18);
 // kevin
 // 18
 // 1
+
+
+
 
 
 // 3.第三版 当是由 null 对象时默认指向 window, 而且有的函数有返回值
@@ -90,3 +97,4 @@ console.log(bar.call2(obj, 'kevin', 18));
 //    name: 'kevin',
 //    age: 18
 // }
+
--- src/topic/gather.js
+++ src/topic/gather.js
@@ -15,11 +15,35 @@ console.log(intersection(arr1, arr2));
 
 
 // 2.实现数组的并集
+// 方法1
 let union = function (arr1, arr2) {
   let unionResult = [].concat(arr1).concat(arr2) // concat 居然可以链式调用诶，神奇！
-  unionResult = [...new Set(unionResult)] // 注意：如果不使用扩展函数的话，结果是个 Set 对象而不是数组
+  unionResult = [...new Set(unionResult)] // 注意：如果不使用扩展函数的话，结果是个 Set 对象而不是数组。同理，也可以用 Array.from
+  // unionResult = Array.from(new Set(unionResult))
   return unionResult
 }
 
 console.log(union(arr1, arr2));
+
+// 方法2：
+let a = [1, 2, 3];
+let b = [2, 4, 5];
+
+let union = a.concat(b.filter(v => !a.includes(v)));
+
+console.log(union); // [ 1, 2, 3, 4, 5 ]
+
+
+
+// 3.差集
+
+let a = [1, 2, 3];
+let b = [2, 4, 5];
+
+let difference = a.concat(b).filter(v => !a.includes(v) || !b.includes(v));
+
+console.log(difference); // [ 1, 3, 4, 5 ]
+
+
+
 
