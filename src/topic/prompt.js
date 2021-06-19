761b3b5816518a2ce6238f141221406442b2b1be
qxl <qinxuelian@bonc.com.cn>
2021-06-19 17:20:08 +0800

    变量提升修改

--- src/topic/prompt.js
+++ src/topic/prompt.js
@@ -19,3 +19,18 @@ function test(){
 console.log(test);
 test = '123'
 console.log(test);
+
+// 3.练习
+console.log(person)
+console.log(fun)
+var person = 'jack'
+console.log(person)
+ 
+function fun () {
+    console.log(person)
+    var person = 'tom'
+    console.log(person)
+}
+fun()
+console.log(person)
+
